85d54ad88e40bdc49ef2e5f4b847816d27920047
voevodovDmitry <mersisay@yandex.ru>
2022-08-29 21:44:35 +0300

    Add task2

--- src/main/java/task2/Runner.java
+++ src/main/java/task2/Runner.java
@@ -1,2 +1,31 @@
-package main.java.task2;public class Runner {
+package main.java.task2;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        int[] array = {5, 6, 3, 2, 5, 1, 4, 9};
+        System.out.println(Arrays.toString(array));
+
+        //select sort
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+
+
+    }
+
 }
+
+
